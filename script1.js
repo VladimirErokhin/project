f42d291a6e0e5b2c51e232fc10bd691a6921a7d9
Vladimir <Vladimir.m.erokhin@gmail.com>
2021-11-21 18:14:07 +0300

    ggg

---
+++ script1.js
@@ -0,0 +1,25 @@
+'use srict';
+
+const str = 'test';
+const arr = [1, 2, 4];
+console.log(str[2]);
+
+console.log(str.toUpperCase());
+
+const fruit = 'Some fruit';
+console.log(fruit.indexOf('fruit'));
+
+const logg = "hello World";
+
+// console.log(logg.slice(0));
+
+// console.log(logg.substring(6, 11));
+
+console.log(logg.substr(2, 3));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = "12.2px";
+// console.log(parseInt(test));
+console.log(parseFloat(test));
